bb7fba8bac73b5174abd1ecb2bdd861e459d3b86
hansmerz <erichhans10@gmail.com>
2021-03-09 22:34:13 -0500

    Upload custom checkout module

---
+++ app/code/Hans/CustomCheckout/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/shipping-information/address-renderer/default.html':
+            'Hans_CustomCheckout/template/shipping-information/address-renderer/default.html'
+        }
+    }
+};
